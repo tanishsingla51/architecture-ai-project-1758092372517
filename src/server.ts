cb0e8e1635e674cf0ab7530ad8bea896cae3c517
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-09-17 12:29:37 +0530

    Add src/server.ts

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import app from './app';
+import config from './config';
+
+const server = app.listen(config.port, () => {
+  console.log(`Server is running on port ${config.port}`);
+});
+
+const exitHandler = () => {
+  if (server) {
+    server.close(() => {
+      console.log('Server closed');
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+};
+
+const unexpectedErrorHandler = (error: unknown) => {
+  console.error(error);
+  exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM received');
+  if (server) {
+    server.close();
+  }
+});
